07ca23fcdb07e7bc9eb7d74c8549a0de1258701a
Russell Cain <russellcain24@gmail.com>
2019-06-15 13:54:09 -0500

    entered decent way for users to input points in the metric space. ClosestPair is techincally done, but could be better.

--- closestPair.py
+++ closestPair.py
@@ -8,7 +8,7 @@
 def distance(p1, p2):
 	dif = 0
 	for i in range(len(p1)):
-		dif += (p1[i]**2 - p2[i]**2)
+		dif += ((p1[i] - p2[i])**2)
 	return dif**(1/2)
 
 class EratosthenesSieve(object):
@@ -29,5 +29,14 @@ class EratosthenesSieve(object):
 
 if __name__ == "__main__":
 	"""Need to find a good way to read in points and group into a list"""
-	points = input("please enter the points in your metric space? \n> ")
+	print("Please enter the points, as (p1,p2,...,p_n) for your metric space. (enter a single '!' when you are done)")
+	newPoint, points = "", []
+	while newPoint != '!':
+		newPoint = input("> ").replace("(", "").replace(")", "")
+		if newPoint != "!":
+			newPoint = newPoint.split(",")
+			for i in range(len(newPoint)):
+				newPoint[i] = float(newPoint[i])
+			points.append(newPoint)
+	# points = [(1,2), (1,3), (5,10)] #testing
 	EratosthenesSieve(points).solution()
